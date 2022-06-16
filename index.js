d9761365236f5dffb9df44eda0feecefad6e80c3
mitchelle <mitchellenjoki7@gmail.com>
2022-06-16 22:59:28 +0300

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  for (let i = 0; i < word.length / 2; i++) {
+    const j = word.length - 1 - i;
+    if (word[i] !== word[j]) return false;
+
+  } 
+  return true 
 }
+
+  // Write your algorithm here
+// }
 
 /* 
   Add your pseudocode here
